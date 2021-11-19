2681dbd32c8302c5a38ef37d9b6c79a525812fcf
Mauricio E Garcia <mgarciamardones@gmail.com>
2021-11-19 00:41:22 -0300

    Base.web

--- application/views/Home.php
+++ application/views/Home.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
-<html lang="en" class="demo-2 no-js">
+<html lang="es">
 
 <head>
 	<!-- Required meta tags -->
@@ -13,136 +13,39 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/component.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="js/parallax.min.js"></script>
-	<script src="js/parallax.js"></script>
-	<script src="js/snap.svg-min.js"></script>
 	<title>Versia.cl</title>
-
+	<script type="text/javascript">
+		var base_url = "<?php echo base_url() ?>index.php/";
+	</script>
 </head>
 
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="#"><img src="./img/versia.icon.new.png" alt="" width="150" height="150"></a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-
-				</li>
-				<li class="nav-item">
-
-				</li>
-				<li class="nav-item dropdown">
-
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-					</div>
-				</li>
-				<li class="nav-item">
-
-				</li>
-			</ul>
-			<ul>
-				<li><a class="font-weight-bold" href="https://api.whatsapp.com/send?phone=56949477660&text=Hola, quiero hacer una consulta."><img src="./img/whatsapp.png" alt="" width="24" height="24"> +56949477660 </a></li>
-				<li><a class="font-weight-bold" href="mailto:hablemos@versia.cl"><img src="./img/correo.png" alt="" width="24" height="24"> hablemos@versia.cl </a></li>
-				<li><a class="font-weight-bold" href="https://www.instagram.com/versiachile/"><img src="./img/insta.png" alt="" width="24" height="24"> versiachile </a></li>
-				<li><a class="font-weight-bold" href="https://www.facebook.com/versiachile/"><img src="./img/face.png" alt="" width="24" height="24"> versiachile </a></li>
-			</ul>
-
-		</div>
+		<a class="icon-nav" href="<?php echo base_url() ?>"></a>
 	</nav>
-	<section class="container">
-	<div class="parallax" data-parallax="scroll" data-image-src="./img/vr_back.jpg"></div>
-	</section>
+	<div class="img-head-body">
 
-	<section id="grid" class="grid clearfix">
-		<a href="#">
-			<figure>
-				<img src="img/oficina_equipada.gif" />
-				<figcaption>
-					<h5>OFICINA EQUIPADA</h5>
-				</figcaption>
-			</figure>
-		</a>
-		<a href="#">
-			<figure>
-				<img src="img/arriendo_equipos.gif" />
-				<figcaption>
-					<h5>ARRIENDO DE EQUIPOS</h5>
-				</figcaption>
-			</figure>
-		</a>
-		<a href="#">
-			<figure>
-				<img src="img/venta_insumos.gif" />
-				<figcaption>
-					<h5>VENTA DE INSUMOS TECNOLÓGICOS</h5>
-				</figcaption>
-			</figure>
-		</a>
-		<a href="#">
-			<figure>
-				<img src="img/armado_gamer.gif" />
-				<figcaption>
-					<h5>ARMADO DE PC GAMER</h5>
-				</figcaption>
-			</figure>
-		</a>
-	</section>
-	<div class="container">
-		<div class="card text-white bg-primary mb-3">
-			<div class="card-header">
-				<h1>OFICINA EQUIPADA</h1>
-			</div>
-			<div class="card-body">
-				<h5 class="card-title">Arriendo de oficina para trabajar de 1 a 3 personas equipada con:</h5>
-				<p class="card-text">
-					<ul>
-						<li>Computador de alta gama</li>
-						<li>Silla de escritorio con cojín lumbar y cojín para el cuello</li>
-						<li>Escritorio amplio con espacio para más de una persona</li>
-						<li>Internet fibra óptica de 400mb de bajada y 200mb de subida</li>
-						<li>Iluminación led</li>
-						<li>Micrófono</li>
-						<li>Audífonos 7.1</li>
-						<li>Web cam 1080p</li>
-						<li>Baño privado</li>
-					</ul>
-				</p>
-				<h5 class="card-title">Valor promocional por apertura: $8.000 por hora, servicio incluye café.</h5>
-			</div>
-		</div>
-		<div class="card text-white bg-success mb-3">
-			<div class="card-header">
-				<h1>ARRIENDO DE EQUIPOS</h1>
-			</div>
-			<div class="card-body">
-				<h5 class="card-title">Arriendo de gafas de realidad virtual Incluye:</h5>
-				<p class="card-text">
-					<ul>
-						<li>Gafas Oculus quest </li>
-						<li>juegos y aplicaciones cargadas en las gafas y computador (sin uso de cables)</li>
-						<li>Asistencia en todo momento</li>
-						<li>Baño privado</li>
-					</ul>
-				</p>
-				<h5 class="card-title">Valor promocional por apertura: $4.000 por hora de uso, incluye 10 minutos para configuración inicial y explicación sobre uso.
-					*en caso de atraso se restarán minutos al tiempo de uso en caso de tener reservada la oficina en el siguiente periodo.</h5>
-			</div>
-		</div>
 	</div>
+
 
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
-			<h1 class="display-4"><img src="./img/versia.icon.new.png" alt="" width="128" height="128"></h1>
-			<p class="lead">Disponibilidad de la oficina: Lunes a viernes de 9am a 19hrs Sábado de 9am a 14hrs</p>
-			<p class="lead">Para reservas o consultas de otros horarios comunicarse vía telefono fijo: (71)-2746423 o vía Whatsapp al número: +56949477660</p>
-			<h5 class="display-5">Puedes Encontrarnos aquí, 30 oriente #1546, Edificio las rastras 2, oficina 309.</h5>
-			<div class="container"></div>
-			<p class="lead"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.0660061985327!2d-71.6286118844558!3d-35.42839488025398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9665c6da184f4319%3A0x2c25275866f3b407!2sCalle%2030%20Ote.%201546%2C%20oficina%20309%2C%20Talca%2C%20Maule!5e0!3m2!1ses-419!2scl!4v1602532033073!5m2!1ses-419!2scl" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
+			<div class="row">
+				<div class="col ">
+					<h1 class="display-4"><img src="./img/img-logo-footer.png" alt="" width="30%"></h1>
+					<p class="lead">Disponibilidad de la oficina: Lunes a viernes de 9am a 19hrs Sábado de 9am a 14hrs</p>
+					<p class="lead">Para reservas o consultas de otros horarios comunicarse vía telefono fijo: (71)-2746423 o vía Whatsapp al número: +56949477660</p>
+					<h5 class="display-5">Puedes Encontrarnos aquí, 30 oriente #1546, Edificio las rastras 2, oficina 309.</h5>
+					<br>
+				</div>
+				<div class="col ">
+					<p class="lead"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.0660061985327!2d-71.6286118844558!3d-35.42839488025398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9665c6da184f4319%3A0x2c25275866f3b407!2sCalle%2030%20Ote.%201546%2C%20oficina%20309%2C%20Talca%2C%20Maule!5e0!3m2!1ses-419!2scl!4v1602532033073!5m2!1ses-419!2scl" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
+					<a class="font-weight-bold" href="https://api.whatsapp.com/send?phone=56949477660&text=Hola, quiero hacer una consulta."><img src="./img/whatsapp.png" alt="" width="24" height="24"> +56949477660 </a>
+					<a class="font-weight-bold" href="mailto:hablemos@versia.cl"><img src="./img/correo.png" alt="" width="24" height="24"> hablemos@versia.cl </a>
+					<a class="font-weight-bold" href="https://www.instagram.com/versiachile/"><img src="./img/insta.png" alt="" width="24" height="24"> versiachile </a>
+					<a class="font-weight-bold" href="https://www.facebook.com/versiachile/"><img src="./img/face.png" alt="" width="24" height="24"> versiachile </a>
+				</div>
+			</div>
 		</div>
 	</div>
 	<!-- Optional JavaScript -->
