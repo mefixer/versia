71a4e056b6897a9eae00e26763caed610d1a1180
Mauricio E Garcia <mgarciamardones@gmail.com>
2021-11-19 01:27:16 -0300

    Carga de bootstrap

--- application/views/Home.php
+++ application/views/Home.php
@@ -10,8 +10,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="shortcut icon" href="./img/versia.icon.short.png">
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<title>Versia.cl</title>
 	<script type="text/javascript">
@@ -21,8 +21,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="icon-nav" href="<?php echo base_url() ?>"></a>
+		<div class="container-fluid">
+			<a class="navbar-brand" href="<?php echo base_url() ?>"><img src="./img/versia.icon.new.png" alt="" width="10%"></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#"><i class="bi bi-bookmark"></i>Inicio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Nosotros</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Servicios</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled">Contactanos</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</nav>
+
 	<div class="img-head-body">
 
 	</div>
@@ -31,28 +53,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col ">
-					<h1 class="display-4"><img src="./img/img-logo-footer.png" alt="" width="30%"></h1>
+				<div class="col">
+					<h1 class="display-2"><img src="./img/versia.icon.short.png" alt="" width="65%" height="auto"></h1>
 					<p class="lead">Disponibilidad de la oficina: Lunes a viernes de 9am a 19hrs Sábado de 9am a 14hrs</p>
 					<p class="lead">Para reservas o consultas de otros horarios comunicarse vía telefono fijo: (71)-2746423 o vía Whatsapp al número: +56949477660</p>
-					<h5 class="display-5">Puedes Encontrarnos aquí, 30 oriente #1546, Edificio las rastras 2, oficina 309.</h5>
+
 					<br>
 				</div>
 				<div class="col ">
-					<p class="lead"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.0660061985327!2d-71.6286118844558!3d-35.42839488025398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9665c6da184f4319%3A0x2c25275866f3b407!2sCalle%2030%20Ote.%201546%2C%20oficina%20309%2C%20Talca%2C%20Maule!5e0!3m2!1ses-419!2scl!4v1602532033073!5m2!1ses-419!2scl" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
-					<a class="font-weight-bold" href="https://api.whatsapp.com/send?phone=56949477660&text=Hola, quiero hacer una consulta."><img src="./img/whatsapp.png" alt="" width="24" height="24"> +56949477660 </a>
-					<a class="font-weight-bold" href="mailto:hablemos@versia.cl"><img src="./img/correo.png" alt="" width="24" height="24"> hablemos@versia.cl </a>
-					<a class="font-weight-bold" href="https://www.instagram.com/versiachile/"><img src="./img/insta.png" alt="" width="24" height="24"> versiachile </a>
-					<a class="font-weight-bold" href="https://www.facebook.com/versiachile/"><img src="./img/face.png" alt="" width="24" height="24"> versiachile </a>
+					<h5 class="display-5">Puedes Encontrarnos aquí, 30 oriente #1546, Edificio las rastras 2, oficina 309.</h5>
+					<br>
+					<p class="lead"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.0660061985327!2d-71.6286118844558!3d-35.42839488025398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9665c6da184f4319%3A0x2c25275866f3b407!2sCalle%2030%20Ote.%201546%2C%20oficina%20309%2C%20Talca%2C%20Maule!5e0!3m2!1ses-419!2scl!4v1602532033073!5m2!1ses-419!2scl" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
 				</div>
+			</div>
+			<div class="row">
+				<div class="col"><a class="font-weight-bold" href="https://api.whatsapp.com/send?phone=56949477660&text=Hola, quiero hacer una consulta."><img src="./img/whatsapp.png" alt="" width="15%" height="auto"> +56949477660 </a></div>
+				<div class="col"><a class="font-weight-bold" href="mailto:hablemos@versia.cl"><img src="./img/correo.png" alt="" width="15%" height="auto"> hablemos@versia.cl </a></div>
+				<div class="col"><a class="font-weight-bold" href="https://www.instagram.com/versiachile/"><img src="./img/insta.png" alt="" width="15%" height="auto"> versiachile </a></div>
+				<div class="col"><a class="font-weight-bold" href="https://www.facebook.com/versiachile/"><img src="./img/face.png" alt="" width="15%" height="auto"> versiachile </a></div>
 			</div>
 		</div>
 	</div>
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
